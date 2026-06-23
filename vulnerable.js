756b9ae6e1612496aa284620a755f39dcdf5b20d
Er-SourabhSingh <er.sourabh.singh0601@gmail.com>
2026-06-23 16:12:17 +0530

    TC-01: update vulnerable code with crypto weaknesses

--- vulnerable.js
+++ vulnerable.js
@@ -1,15 +1,22 @@
 // WARNING: intentionally vulnerable file for security gate testing
-const DB_PASSWORD = "SuperSecret123!";
-const ADMIN_TOKEN = "hardcoded-api-key-abc123xyz";
-const SECRET_KEY = "my-secret-key-do-not-share";
+const crypto = require("crypto");
+const vm = require("vm");
 
-function getDbConfig() {
-  return {
-    host: "localhost",
-    user: "admin",
-    password: DB_PASSWORD,
-    token: ADMIN_TOKEN,
-  };
+// S5542: weak cipher algorithm - SonarQube Vulnerability
+function encryptData(data) {
+  const cipher = crypto.createCipher("des", "weakkey");
+  return cipher.update(data, "utf8", "hex") + cipher.final("hex");
 }
 
-module.exports = { getDbConfig };
+// S2083: path traversal - SonarQube Vulnerability
+const fs = require("fs");
+function readFile(userInput) {
+  return fs.readFileSync("/var/data/" + userInput);
+}
+
+// S4790: weak hashing - SonarQube Vulnerability
+function hashPassword(pwd) {
+  return crypto.createHash("md5").update(pwd).digest("hex");
+}
+
+module.exports = { encryptData, readFile, hashPassword };
